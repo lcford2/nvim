6febcb2122bd95d9ba874bf2145785405de67b97
Lucas Ford <lcford2@ncsu.edu>
2024-01-07 16:22:24 -0500

    improve install behavior for linux

--- install.sh
+++ install.sh
@@ -3,17 +3,18 @@
 # ubuntu deps ninja-build gettext cmake unzip curl git gcc
 # macos xcode-select --install, brew, ninja, cmake, gettext, curl
 
-MAJOR_TARGET=9
-MINOR_TARGET=5
-VERSION="v0.${MAJOR_TARGET}.${MINOR_TARGET}"
+LATEST_RELEASE=v0.0.0
+
+NVIM_MAJOR_TARGET=9
+NVIM_MINOR_TARGET=5
+VERSION="v0.${NVIM_MAJOR_TARGET}.${NVIM_MINOR_TARGET}"
 BASE_URL="https://github.com/neovim/neovim/releases/download/${VERSION}"
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 
 ARCH=$(uname -m)
 OS=$(uname)
 
-if [ -z "${PREFIX}" ]; then
-  PREFIX=/usr/local
+if [ -z "${NVIM_PREFIX}" ]; then
+  NVIM_PREFIX=/usr/local
 fi
 
 INSTALL=1
@@ -34,58 +35,115 @@ neovim_install_help() {
   echo "for installation instructions."
 }
 
+_install_deps_apt () {
+  # Use APT to install the build dependencies for neovim
+  if command -v sudo &>/dev/null; then
+    SUDO_STR="sudo "
+  else
+    SUDO_STR=""
+  fi
+
+  ${SUDO_STR}apt update && ${SUDO_STR}apt install -y \
+    curl \
+    git \
+    python3-venv \
+    python3-pip \
+    clang
+}
+
+_install_build_deps_apt () {
+  # Use APT to install the build dependencies for neovim
+  if command -v sudo &>/dev/null; then
+    SUDO_STR="sudo "
+  else
+    SUDO_STR=""
+  fi
+
+  ${SUDO_STR}apt update && ${SUDO_STR}apt install -y \
+    build-essential \
+    cmake \
+    gettext \
+    unzip
+}
+
 _install_binary () {
+  # Download and install prebuilt binaries
   echo "INTALLING FROM BINARY"
+  # ensure an argument was passed
   if [ $# -lt 1 ]; then
     echo "Must provide binary name."
     exit 3
   fi
+  # download neovim binaries
   binary_name="$1"
   URL="${BASE_URL}/${binary_name}.tar.gz"
-  echo "$URL"
   curl -sOL "$URL"
+  # extract and install neovim components
   tar xf "${binary_name}.tar.gz"
-  mkdir -p "${PREFIX}/{bin,share,lib,man}"
-  cp -r "${binary_name}/bin/nvim" "${PREFIX}/bin/nvim"
-  cp -r "${binary_name}/share/"* "${PREFIX}/share"
-  cp -r "${binary_name}/lib/"* "${PREFIX}/lib"
-  cp -r "${binary_name}/man/"* "${PREFIX}/man"
+  mkdir -p "${NVIM_PREFIX}/{bin,share,lib,man}"
+  cp -r "${binary_name}/bin/nvim" "${NVIM_PREFIX}/bin/nvim"
+  cp -r "${binary_name}/share/"* "${NVIM_PREFIX}/share"
+  cp -r "${binary_name}/lib/"* "${NVIM_PREFIX}/lib"
+  cp -r "${binary_name}/man/"* "${NVIM_PREFIX}/man"
   rm -rf "./${binary_name}" "./${binary_name}.tar.gz"
 }
 
 _install_source () {
   echo "INTALLING FROM SOURCE"
+  # install build dependencies
+  if ! command -v apt &>/dev/null; then
+    echo "Cannot install neovim build deps using apt (apt not found)"
+    exit 4
+  fi
+  _install_build_deps_apt
+  # download and extract target release
   curl -sOL "https://github.com/neovim/neovim/archive/refs/tags/${VERSION}.tar.gz"
   tar xf "${VERSION}.tar.gz"
-  cd "neovim-0.${MAJOR_TARGET}.${MINOR_TARGET}"
-  make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$PREFIX"
+  cd "neovim-0.${NVIM_MAJOR_TARGET}.${NVIM_MINOR_TARGET}" || exit 2
+  # build neovim and install
+  make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_NVIM_PREFIX=$NVIM_PREFIX"
   make install || \
     echo "ERROR: Could not run 'make install' for neovim." \
          " This is likely due to permission errors. Try running " \
          " with sudo."
 }
 
+_install_node () {
+  # install nodejs for lsps
+  nvm_install_output=$(curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash)
+  nvm_dir_cmd=$(echo "$nvm_install_output" | grep -oP "export NVM_DIR=\H*")
+  eval "$nvm_dir_cmd"
+  if [ -n "$NVM_DIR" ]; then
+    source "$NVM_DIR/nvm.sh"
+    nvm install node
+  else
+    echo "Could not install node with nvm."
+  fi
+}
+
 install_nvim () {
+  _install_deps_apt
   if [ "${OS}" == "Linux" ]; then
     if [ "${ARCH}" != "amd64" ]; then
+      # Install neovim from source since no precompiled binaries exist
       _install_source
-      # echo "Cannot install pre-compiled binaries for arm64 on Linux."
-      # echo "Download source code and compile Neovim from source."
-      # neovim_install_help
-      # exit 1
     else 
       # Install neovim from latest stable release
       _install_binary "nvim-linux64"
     fi
   elif [ "${OS}" == "Darwin" ]; then
     if command -v brew &>/dev/null; then
+      # prefer brew install 
       brew install neovim
     else
+      # if no brew, download binaries and install them
       _install_binary "nvim-macos"
     fi
   fi
+  _install_node
 }
 
+# parse command line arguments
 while test $# -gt 0; do
   case "$1" in
     -n|--no-install)
@@ -108,8 +166,9 @@ echo "Installing with configuration:"
 echo "Neovim Version: $VERSION"
 echo "ARCH: ${ARCH}"
 echo "OS: ${OS}"
-echo "PREFIX: ${PREFIX}"
+echo "NVIM_PREFIX: ${NVIM_PREFIX}"
 
+# If neovim is not found, download it
 if ! command -v nvim &> /dev/null; then
   if [ "$INSTALL" == 0 ]; then
     echo "Neovim was not found and no install attempt was made."
@@ -120,18 +179,19 @@ if ! command -v nvim &> /dev/null; then
   install_nvim 
 fi
 
+# ensure the XDG_CONFIG_HOME variable is set
 if [ -z "$XDG_CONFIG_HOME" ]; then
   mkdir -p ~/.config
   XDG_CONFIG_HOME="${HOME}/.config"
 fi
 
 NVIM_DIR="${XDG_CONFIG_HOME}/nvim"
-
+# backup old neovim settings
 if [ -d "${NVIM_DIR}" ]; then
   mv "${NVIM_DIR}" "${NVIM_DIR}.bkp"
 fi
 
-mkdir -p "${NVIM_DIR}"
-curl -sOL 
-
-
+# get neovim configuration
+curl -sOL "https://github.com/lcford2/nvim/archive/refs/tags/${LATEST_RELEASE}.tar.gz"
+tar xf "${LATEST_RELEASE}.tar.gz"
+mv "nvim-${LATEST_RELEASE//v}" "${NVIM_DIR}"
